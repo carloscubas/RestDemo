5ba0f0686fc597e78cbf51a91621becf56f3d57c
carloscubas <carlos.cubas@gmail.com>
2017-10-06 13:10:58 -0300

    Ajuste nos codigos

--- app/src/main/java/br/com/rest/api/ApiServices.java
+++ app/src/main/java/br/com/rest/api/ApiServices.java
@@ -1,12 +1,9 @@
 package br.com.rest.api;
 
-import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.POST;
 
 /**
  * Created by cubas on 06/10/17.
--- app/src/main/java/br/com/rest/api/Contatos.java
+++ app/src/main/java/br/com/rest/api/Contatos.java
@@ -3,10 +3,6 @@ package br.com.rest.api;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by cubas on 06/10/17.
- */
-
 public class Contatos {
 
     private List<Contato> contatos = new ArrayList<>();
